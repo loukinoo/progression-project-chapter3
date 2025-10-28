b6f3ed8b0cff561324ca1cecc53572c788f2db4c
loukinoo <lucabagins@gmail.com>
2025-10-28 16:03:01 +0100

    Added a file for testing the functions created

---
+++ populating-db/testing-functions.sql
@@ -0,0 +1,10 @@
+SELECT create_task(1, 'Testing');
+SELECT create_task(1, 'To delete');
+
+SELECT assign_task_to(1, 6, 3);
+
+SELECT mark_completed(3, 6);
+
+SELECT remove_task(1, 8);
+
+SELECT completion_rate_of(1, 2), own_completion_rate(3), see_own_tasks(2), total_completion_rate(1);
